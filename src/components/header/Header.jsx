2654c91b9995765bfbf8bef16b0fcbb7aa49a652
JFujitaD <fujitajulian@icloud.com>
2022-11-17 10:46:53 -0700

    Styling and icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,11 @@
-import './Header.css'
-import { Grid, Paper, Typography } from "@mui/material";
+import './Header.css';
+import { Divider, Grid, IconButton, Paper, Typography } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
-const jobTitle = 'Software Engineer';
+const jobTitle = 'Full Stack Developer';
+const company = '309th Software Engineering Group';
 const description = 'Logical, organized, fast-paced programmer with expertise in Java, Python, JavaScript, and C#. Skilled in several areas of programming such as object-oriented programming, databases, containerization technologies, event-driven programming, graphical user interfaces, and more. Experienced in systems engineering and weapon system acquisition through the use of the Scaled Agile Framework.';
 
 const Header = () => {
@@ -17,12 +21,26 @@ const Header = () => {
                 <Grid container spacing={2}>
                     <Grid item xs={7}>
                         <Typography variant='h3'>Julian Dean</Typography>
-                        <Typography variant='subtitle1'>{jobTitle}</Typography>
+                        <Typography variant='h5'>{jobTitle}</Typography>
+                        <Typography variant='subtitle1'>{company}</Typography>
+                        <IconButton>
+                            <GitHubIcon />
+                        </IconButton>
+                        <IconButton>
+                            <LinkedInIcon />
+                        </IconButton>
+                        <IconButton>
+                            <EmailIcon />
+                        </IconButton>
                     </Grid>
                     <Grid item xs={5}>
-                        <img src='profile.png' />
+                        <img src='profile.png' alt='Profile'/>
                     </Grid>
                     <Grid item xs={12}>
+                        <Divider style={{ width: '100%' }} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant='h5'>About</Typography>
                         <Typography variant='body'>{description}</Typography>
                     </Grid>
                 </Grid>
