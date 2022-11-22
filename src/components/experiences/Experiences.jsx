62e248ad88cb3a468664cb2573ae8bfeb447575a
JFujitaD <fujitajulian@icloud.com>
2022-11-21 17:51:49 -0700

    Refactored code

--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -1,19 +1,14 @@
 import { Paper, Typography, Grid, Accordion, AccordionSummary, AccordionDetails, useTheme } from "@mui/material";
-import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
-import CodeIcon from '@mui/icons-material/Code';
 import ConstructionIcon from '@mui/icons-material/Construction';
 import experiencesData from './experiencesData.json';
 import './Experiences.css';
 import { useContext } from "react";
 import AppContext from "../../AppContext";
 import ReactVisibilitySensor from "react-visibility-sensor";
+import Experience from "./experience/Experience";
 
 const Experiences = () => {
     const appContext = useContext(AppContext);
-    const iconMapping = [
-        <CodeIcon />,
-        <IntegrationInstructionsIcon />,
-    ];
     const theme = useTheme();
 
     return (
@@ -32,58 +27,21 @@ const Experiences = () => {
             >
                 <Grid container spacing={5}>
                     <Grid item xs={12}>
-                        <Typography variant='h3'>Experience</Typography>
+                        <Typography variant='h3'>Experience and Skills</Typography>
                     </Grid>
                     <Grid item xs={6}>
                         {experiencesData.industry.map((e) => {
                             return (
-                                <Accordion key={e.name}>
-                                    <AccordionSummary onClick={() => appContext.updateProgress(e.title)}>
-                                        <Grid container>
-                                            <Grid item xs={11}>
-                                                <Typography variant='h5'>{e.title}</Typography>
-                                            </Grid>
-                                            <Grid item xs={1} sx={{ textAlign: 'right' }}>
-                                                {iconMapping[e.id]} 
-                                            </Grid>
-                                        </Grid>
-                                    </AccordionSummary>
-                                    <AccordionDetails>
-                                        <Accordion sx={{ backgroundColor: theme.palette.background.paper2 }}>
-                                            <AccordionSummary onClick={() => appContext.updateProgress(e.name)}>
-                                                <Typography variant='subtitle1'>{e.name}</Typography>
-                                            </AccordionSummary>
-                                            <AccordionDetails>
-                                                <Typography variant='subtitle2'>{e.description}</Typography>
-                                            </AccordionDetails>
-                                        </Accordion>
-                                        <Accordion sx={{ marginTop: '1em', backgroundColor: theme.palette.background.paper2 }}>
-                                            <AccordionSummary onClick={() => appContext.updateProgress(`${e.title}-skills`)}>
-                                                <Typography variant='subtitle1'>Skills</Typography>
-                                            </AccordionSummary>
-                                            <AccordionDetails>
-                                                <ul className="List">
-                                                    {e.tasks.map((s) => {
-                                                        return (
-                                                                <li key={s}>
-                                                                    <Typography variant='subtitle2'>{s}</Typography>
-                                                                </li>
-                                                        );
-                                                    })}
-                                                </ul>
-                                            </AccordionDetails>
-                                        </Accordion>
-                                    </AccordionDetails>
-                                </Accordion>
+                                <Experience key={e.name} e={e} />
                             );
                         })}
                     </Grid>
                     <Grid item xs={6}>
                         <Accordion>
-                            <AccordionSummary onClick={() => appContext.updateProgress('technologies')}>
+                            <AccordionSummary onClick={() => appContext.updateProgress('skills')}>
                             <Grid container>
                                 <Grid item xs={11}>
-                                    <Typography variant='h5'>Technologies</Typography>
+                                    <Typography variant='h5'>Skills</Typography>
                                 </Grid>
                                 <Grid item xs={1} sx={{ textAlign: 'right' }}>
                                     <ConstructionIcon /> 
@@ -92,7 +50,7 @@ const Experiences = () => {
                             </AccordionSummary>
                             <AccordionDetails>
                                 <ul className="List">
-                                    {experiencesData.technologies.map((t) => {
+                                    {experiencesData.skills.map((t) => {
                                         return (
                                             <li key={t}>
                                                 <Typography variant='subtitle1'>{t}</Typography>
